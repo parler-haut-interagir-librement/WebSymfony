7ac74d0342e874c2cec09c5841f99e7f89bf7282
Philippe Gamache <info@ph-il.ca>
2023-09-14 20:16:32 -0400

    Add KnpMenu

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
 ];
---
+++ src/Menu/MainMenuBuilder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+final readonly class MainMenuBuilder
+{
+
+    /**
+     * Add any other dependency you need...
+     */
+    public function __construct(private FactoryInterface $factory, private TranslatorInterface $translator)
+    {
+    }
+
+    public function createMainMenu(array $options): ItemInterface
+    {
+        $menu = $this->factory->createItem('main', $options);
+
+        $menu->addChild('menu.home.text',
+                        [
+                            'route'          => 'homepage',
+                            'linkAttributes' => ['title' => $this->translator->trans('menu.home.title')],
+                        ]
+        );
+        $menu->addChild('menu.stimulus.text',
+                        [
+                            'route'          => 'app_stimulus',
+                            'linkAttributes' => ['title' => $this->translator->trans('menu.stimulus.title')],
+                        ]
+        );
+        $menu->addChild('menu.error.text',
+                        [
+                            'uri'          => '/404',
+                            'linkAttributes' => ['title' => $this->translator->trans('menu.error.title')],
+                        ]
+        );
+
+        return $menu;
+    }
+
+}
