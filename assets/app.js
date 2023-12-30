d23c5372dc7dbf79af2513391501d9af68686cba
Philippe Gamache <info@ph-il.ca>
2023-12-29 22:07:22 -0500

    Update asset

--- assets/app.js
+++ assets/app.js
@@ -4,8 +4,6 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-
-import './bootstrap.js';
 import './styles/app.css'
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
